74b044d6606c483a3f8de5a8335a55c7c707dcb9
Anson W. Han <ansonwhan@gmail.com>
2017-12-25 09:29:34 -0500

    initialize vagrant/blt/drupal sandbox repo

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['d8-blt.local'] = array(
+  'root' => '/var/www/d8-blt/docroot',
+  'uri' => 'http://local.d8-blt.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['d8-blt.local'] += array(
+    'remote-host' => 'local.d8-blt.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
